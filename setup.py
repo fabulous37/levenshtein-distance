138ae48cb7af6e5834bf1420ab2bd90bca1032f6
Lavigne C <82817422+fabulous37@users.noreply.github.com>
2024-04-30 02:12:03 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Streaming video data via networks'
+
+# Setting up
+setup(
+    name="vidstream",
+    version=VERSION,
+    author="Camille Lavigne",
+    author_email="",
+    description=DESCRIPTION,
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['opencv-python', 'pyautogui', 'pyaudio'],
+    keywords=['python', 'levenshtein distance', 'edit type', 'error type'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
